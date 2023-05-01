3d4d02c36fe207bb4074b0f772859070500dbcad
Javad Asgari Shafique <me@javad.sh>
2023-05-01 15:24:26 +0200

    Tilføj comparable interface på UgeDato

--- src/main/java/internal/UgeDato.java
+++ src/main/java/internal/UgeDato.java
@@ -1,6 +1,8 @@
 package internal;
 
-public class UgeDato {
+import io.cucumber.java.sl.In;
+
+public class UgeDato implements Comparable<UgeDato> {
     int årstal;
     int uge;
 
@@ -31,5 +33,13 @@ public class UgeDato {
         int difference = Math.abs(this.årstal - dato.årstal) * 52 + Math.abs(this.uge - dato.uge) + 1;
         assert(difference >= 1);
         return difference;
+    }
+
+    @Override
+    public int compareTo(UgeDato o) {
+        // For at sammenligne UgeDato kan først sammenligne årstal, og hvis de er ens, ugen.
+        return this.årstal == o.årstal ?
+                Integer.compare(this.uge, o.uge) :
+                Integer.compare(this.årstal, o.årstal);
     }
 }
