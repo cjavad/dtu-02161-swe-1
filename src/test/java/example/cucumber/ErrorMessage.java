ad12a1aba459bb1773bc83c45803216b9f01c6b7
s215773 <magnus.a.mccubbin@gmail.com>
2023-05-07 16:05:21 +0200

    error message

---
+++ src/test/java/example/cucumber/ErrorMessage.java
@@ -0,0 +1,19 @@
+package example.cucumber;
+
+public class ErrorMessage {
+
+    public String message;
+
+    public ErrorMessage() {
+        this.message = "";
+    }
+
+    public String getMessage() {
+        return this.message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+}
