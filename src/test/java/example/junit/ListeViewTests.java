7b3530a4ffe57bfda97bddbb4d92021db68a9ba2
anon <Hjalte Nannestad>
2023-05-08 13:11:51 +0200

    SystemAppTests

--- src/test/java/example/junit/ListeViewTests.java
+++ src/test/java/example/junit/ListeViewTests.java
@@ -3,6 +3,8 @@ package	example.junit;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.junit.Assert;
+
 import internal.ListeView;
 
 public class ListeViewTests {
@@ -25,31 +27,31 @@ public class ListeViewTests {
 	@org.junit.Test()
 	public void testFraVenstreTilHøjre() {
 		listeView.fraVenstreTilHøjre("B");
-		assert listeView.højreListe.contains("B");
-		assert !listeView.venstreListe.contains("B");
+		Assert.assertTrue(listeView.højreListe.contains("B"));
+		Assert.assertFalse(listeView.venstreListe.contains("B"));
 	}
 
 	@org.junit.Test()
 	public void testFraHøjreTilVenstre() {
 		listeView.fraHøjreTilVenstre("C");
-		assert listeView.venstreListe.contains("C");
-		assert !listeView.højreListe.contains("C");
+		Assert.assertTrue(listeView.venstreListe.contains("C"));
+		Assert.assertFalse(listeView.højreListe.contains("C"));
 	}
 
 	@org.junit.Test()
 	public void testGetHøjreListe() {
-		assert listeView.getHøjreListe().equals(listeView.højreListe);	
+		Assert.assertTrue(listeView.getHøjreListe().equals(listeView.højreListe));
 	}
 
 	@org.junit.Test()
 	public void testGetVenstreListe() {
-		assert listeView.getVenstreListe().equals(listeView.venstreListe);	
+		Assert.assertTrue(listeView.getVenstreListe().equals(listeView.venstreListe));
 	}
 
 	@org.junit.Test()
 	public void testValgteObjekt() {
 		listeView.setValgteObjekt("A");
-		assert listeView.getValgteObjekt().equals("A");
+		Assert.assertTrue(listeView.getValgteObjekt().equals("A"));
 	}
 
 	@org.junit.Test()
@@ -57,7 +59,7 @@ public class ListeViewTests {
 		List<String> højre = new ArrayList<>();
 		højre.add("E");
 		listeView.setHøjreListe(højre);
-		assert listeView.getHøjreListe().equals(højre);
+		Assert.assertTrue(listeView.getHøjreListe().equals(højre));
 	}
 
 	@org.junit.Test()
@@ -65,6 +67,6 @@ public class ListeViewTests {
 		List<String> venstre = new ArrayList<>();
 		venstre.add("F");
 		listeView.setVenstreListe(venstre);
-		assert listeView.getVenstreListe().equals(venstre);
+		Assert.assertTrue(listeView.getVenstreListe().equals(venstre));
 	}
 }
---
+++ src/test/java/example/junit/SystemAppTests.java
@@ -0,0 +1,55 @@
+package example.junit;
+
+import org.junit.Assert;
+
+import internal.Medarbejder;
+import internal.Projekt;
+import internal.SystemApp;
+
+public class SystemAppTests {
+	SystemApp systemApp;
+
+	@org.junit.Before()
+	public void testConstructor() {
+		systemApp = new SystemApp();
+		Assert.assertTrue(systemApp.login("admin"));
+	}
+
+	@org.junit.Test()
+	public void testTilføjProjekt() {
+		try {
+			Assert.assertTrue(systemApp.lavNytProjekt("Projekt 1"));
+		} catch (Exception e) {
+			Assert.assertTrue(false);
+		}
+	}
+
+	@org.junit.Test()
+	public void testSletProjekt() {
+		try {
+			Assert.assertTrue(systemApp.lavNytProjekt("Projekt 1"));
+		} catch (Exception e) {
+			Assert.assertTrue(false);
+		}
+
+		Projekt projekt = systemApp.findProjektMedNavn("Projekt 1");
+
+		try {
+			systemApp.sletProjekt(projekt);
+		} catch (Exception e) {
+			Assert.assertTrue(false);
+		}
+	}
+
+	@org.junit.Test()
+	public void testTilføjMedarbejder() {
+		systemApp.opretNyMedarbejder("MEB");
+	}
+
+	@org.junit.Test()
+	public void testSletMedarbejder() {
+		systemApp.opretNyMedarbejder("MEB");
+		Medarbejder medarbejder = systemApp.findMedarbejder("MEB");
+		systemApp.sletMedarbejder(medarbejder);
+	}
+}
