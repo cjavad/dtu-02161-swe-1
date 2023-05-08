62740d46b2b453e793d54b8ecd2a6ee702632c5d
anon <Hjalte Nannestad>
2023-05-08 14:31:53 +0200

    contracts

--- src/test/java/example/junit/AktivitetTests.java
+++ src/test/java/example/junit/AktivitetTests.java
@@ -1,7 +1,11 @@
 package example.junit;
 
+import org.junit.Assert;
+
 import internal.Aktivitet;
+import internal.Medarbejder;
 import internal.Projekt;
+import internal.UgeDato;
 
 public class AktivitetTests {
 	Aktivitet aktivitet;
@@ -17,5 +21,33 @@ public class AktivitetTests {
 		aktivitet.setProjekt(new Projekt("Projekt 1", "et id"));
 		aktivitet.getBugetteretTid();
 		aktivitet.toString();
+	}
+
+	@org.junit.Test()
+	public void testTilføjMedarbejder() {
+		Assert.assertThrows(AssertionError.class, () -> {
+		    aktivitet.tilføjMedarbjeder(null);
+		});
+
+		Medarbejder medarbejder = new Medarbejder("MEB");
+		Assert.assertThrows(AssertionError.class, () -> {
+		    aktivitet.tilføjMedarbjeder(medarbejder);
+		});
+
+		Projekt projekt = new Projekt("Projekt 1", "et id");
+
+		Assert.assertThrows(AssertionError.class, () -> {
+		    aktivitet.tilføjMedarbjeder(medarbejder);
+		});
+
+
+		aktivitet.setProjekt(projekt);
+		projekt.tilføjMedarbejder(medarbejder);
+		aktivitet.tilføjMedarbjeder(medarbejder);
+	}
+
+	@org.junit.Test()
+	public void testIsLegalDatoAssignment() {
+		Assert.assertTrue(aktivitet.isLegalDatoAssignment(new UgeDato(2017, 1), new UgeDato(2018, 1)));
 	}
 }
