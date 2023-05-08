521c8f706f61223fcfaced3168348270102e1219
Javad Asgari Shafique <me@javad.sh>
2023-05-08 15:06:43 +0200

    Added wbt for FjernMedarbejderFraProjektTests

---
+++ src/test/java/example/junit/whitebox/FjernMedarbejderFraProjektTests.java
@@ -0,0 +1,41 @@
+package example.junit.whitebox;
+
+import internal.Medarbejder;
+import internal.Projekt;
+import internal.SystemApp;
+import internal.SystemAppException;
+import org.mockito.internal.matchers.Null;
+
+public class FjernMedarbejderFraProjektTests {
+    SystemApp systemApp;
+    Medarbejder m;
+    Projekt projekt;
+
+    @org.junit.Before()
+    public void setup() throws SystemAppException {
+        systemApp = new SystemApp();
+        systemApp.login("admin");
+        m = new Medarbejder("aaaa");
+        systemApp.lavNytProjekt("Projekt 1");
+        projekt = systemApp.findProjektMedNavn("Projekt 1");
+        systemApp.tilføjMedarbejderTilProjekt(m, projekt);
+        systemApp.lavAktivitet("Aktivit 1", projekt);
+    }
+
+    @org.junit.Test()
+    public void inputsetA() {
+        org.junit.Assert.assertThrows(NullPointerException.class, () -> systemApp.planner.fjernMedarbejderFraProjekt(null, projekt));
+    }
+
+    @org.junit.Test()
+    public void inputsetB() {
+        org.junit.Assert.assertThrows(NullPointerException.class, () -> systemApp.planner.fjernMedarbejderFraProjekt(m, null));
+    }
+
+    @org.junit.Test()
+    public void inputsetC() {
+        systemApp.planner.fjernMedarbejderFraProjekt(m, projekt);
+        org.junit.Assert.assertTrue(m.getProjekter().isEmpty());
+        org.junit.Assert.assertTrue(projekt.getMedarbejder().isEmpty());
+    }
+}
